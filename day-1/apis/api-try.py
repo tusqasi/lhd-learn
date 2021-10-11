7724634ea087f98d916562864c16bbe77c9ba188
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-10-11 21:13:16 +0530

    working with apis

--- day-1/apis/api-try.py
+++ day-1/apis/api-try.py
@@ -1,6 +1,13 @@
 import requests
+from pprint import pprint as print
 
-url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt"
-joke = requests.get(url).text
+joke_url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&format=txt"
+translate_url = "https://api.funtranslations.com/translate/gungan.json"
+joke = requests.get(joke_url).text
+print(joke.strip("'"))
+params_translate= {
+        "text":joke
+        }
+translated = requests.get(translate_url,params=params_translate)
 
-print(joke)
+print(translated)
