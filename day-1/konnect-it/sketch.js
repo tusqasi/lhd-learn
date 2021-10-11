b6262fe4d2b22fc954a003880c0c2413e91ee279
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-10-11 20:06:52 +0530

    works

--- day-1/konnect-it/sketch.js
+++ day-1/konnect-it/sketch.js
@@ -1,19 +1,50 @@
+// the game board
+let cellSize;
+let board;
+let cellX;
+let cellY;
+let cellPressedX;
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1330, 800);
+  cellSize = 200;
+  cellX = floor(width / cellSize);
+  cellY = floor(height / cellSize);
+  board = [];
+  for (let i = 0; i < cellX; i++) {
+    board.push([0]);
+  }
+}
+
+function mouseClicked(e) {
+  cellPressedX = floor(mouseX / cellSize);
+  board[cellPressedX].push(1);
 }
 
 function draw() {
-  drawGrid(4)
-  // background(0);
-
+  background(122);
+  drawGrid();
+	drawBoard();
 }
 
-function drawGrid(n) {
-  let cellSize = width / n;
-  for (let i = 1; i < n + 1; i++) {
-    // veritcal
-    line(cellSize * (i), 0, cellSize * i, width);
-    // horizontal
-    line(0, cellSize * i, width, cellSize * i);
+function drawGrid() {
+  fill(240, 200, 0);
+  rect(0, 0, cellX * cellSize, cellY * cellSize);
+
+  // vertical
+  for (let i = 1; i < cellX + 1; i++) {
+    line(cellSize * i, 0, cellSize * i, cellY * cellSize);
+  }
+  // console.log(y * cellSize);
+  for (let i = 1; i < cellY + 1; i++) {
+    line(0, cellSize * i, cellX * cellSize, cellSize * i);
+  }
+}
+
+function drawBoard() {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      
+    }
+      // console.log("\n");
   }
 }
