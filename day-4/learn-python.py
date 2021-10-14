3fcd375d91b61085118de77598478669b51cd15c
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-10-14 20:43:15 +0530

    added things and corrections

--- day-4/learn-python.py
+++ day-4/learn-python.py
@@ -1,53 +1,47 @@
-
-#task : dict
-
-
 # adds a task to tasks dict
-def add_task(task_name, task_desc, tasks):
-
+def add_task(task_name: str, task_desc: str, tasks: dict) -> dict:
     tasks[task_name] = task_desc
     return tasks
 
 
 # prints all tasks in `tasks`
-def view_tasks(tasks):
+def view_tasks(tasks: dict) -> None:
     for key, value in tasks.items():
-        print(f"name: {key}\ndescription: {value}\n")
+        print(f"Name: {key}\nDescription: {value}\n")
+
 
 # removes given tasks
-def remove_task(task_name, tasks):
+def remove_task(task_name: str, tasks: dict) -> dict:
     del tasks[task_name]
     return tasks
 
 
 def main():
-
+    tasks: dict = {}
     while True:
+
         # take input to check what action to do
         action = input(
             "What do you want to do?\
-        \n A) Add task\
-        \n V) View tasks\
-        \n R) Remove tasks")
-
+          \nA) Add task \
+          \nV) View tasks\
+          \nR) Remove tasks\n"
+        )
+        view_tasks(tasks)
         # check input from user
-        if (action == "a") or (action == "A"): # when action is 'a' or 'A' this is run
-
-            task_name = input("Give task name")
-            task_desc = input("Give task description")
-            
+        if (action == "a") or (action == "A"):  # when action is 'a' or 'A' this is run
+            task_name = input("Give task name to add: ")
+            task_desc = input("Give task description: ")
             tasks = add_task(task_name, task_desc, tasks)
-            tasks = view_tasks(tasks)
+            view_tasks(tasks)
+        elif (action == "v") or (action == "V"):
+            view_tasks(tasks)
+        elif (action == "r") or (action == "R"):
+            task_name = input("Give task name to remove")
+            tasks = remove_task(task_name, tasks)
+        else:
+            print("Wrong input given")
 
 
-def testing():
-    tasks = {
-        "task_one": "make a todo list app",
-        "task_two": "learn python",
-    }
-    view_tasks(tasks)
-    tasks = remove_task("task_one", tasks)
-    view_tasks(tasks)
-
-
-testing()
+if __name__ == "__main__":
+    main()
